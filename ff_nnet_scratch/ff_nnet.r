6358619f4229bc611aedaa0473e258eb88da500a
surh <sur00mx@gmail.com>
2025-09-01 13:44:39 -0600

    Simplified version

--- ff_nnet_scratch/ff_nnet.r
+++ ff_nnet_scratch/ff_nnet.r
@@ -41,14 +41,6 @@ Dat2 %>%
 
 
 #' # Feed forward neural network
-#' 
-
-#' Set up network architecture
-# L <- 3 # Number of layers
-
-#' Setup data
-A_0 <- t(Dat2[,c("x1", "x2")])
-Y <- matrix(Dat2$y, nrow = 1)
 
 #' Define cost
 cross_entropy_binom_cost <- function(y, y_hat){
@@ -63,15 +55,6 @@ g_logistic <- function(x){
   
   return(a)
 }
-
-#' Initialize network
-n = c(2, 3, 3, 1) # Nodes per layer (and input)
-W <- list(W_1 = matrix(rnorm(n = n[2] * n[1]), nrow = n[2], ncol = n[1]),
-          W_2 = matrix(rnorm(n = n[3] * n[2]), nrow = n[3], ncol = n[2]),
-          W_3 = matrix(rnorm(n = n[4] * n[3]), nrow = n[4], ncol = n[3]))
-B <- list(B_1 = matrix(rnorm(n = n[2]), nrow = n[2], ncol = 1),
-          B_2 = matrix(rnorm(n = n[3]), nrow = n[3], ncol = 1),
-          B_3 = matrix(rnorm(n = n[4]), nrow = n[4], ncol = 1))
 
 #' Feed forward over three layers
 ff_nnet <- function(A_0, W, B, g = match.fun(g_logistic)){
@@ -98,40 +81,32 @@ ff_nnet <- function(A_0, W, B, g = match.fun(g_logistic)){
 # cross_entropy_binom_cost(Y, ff_nnet(A_0 = A_0, W = W, B = B, g = g_logistic)$Y_hat)
 
 #' Do backpropagation by layer
-backpropagation_3 <- function(Y_hat, Y, m, A_2, W_3){
+backpropagation <- function(Y_hat, Y, m, A_2, A_1, A_0, W_3, W_2, W_1){
+  # Layer 3
   A_3 <- Y_hat
   
   dC_dZ3 <- (1/m) * (A_3 - Y)
-  
   dZ3_dW3 <- A_2
   dC_dW3 <- dC_dZ3 %*% t(dZ3_dW3)
-
+  
   dZ3_dA2 <- W_3 
   dC_dA2 <- t(W_3) %*% dC_dZ3
   
   dC_dB3 <- matrix(rowSums(dC_dZ3), ncol = 1)
   
-  return(list(dC_dW3 = dC_dW3, dC_dB3 = dC_dB3, dC_dA2 = dC_dA2))
-}
-
-backpropagation_2 <- function(dC_dA2, A_1, A_2, W_2){
-  
+  # Layer 2
   dA2_dZ2 <- A_2 * (1 - A_2)
   dC_dZ2 <- dC_dA2 * dA2_dZ2
   
   dZ2_dW2 <- A_1
   dC_dW2 <- dC_dZ2 %*% t(dZ2_dW2)
-
+  
   dZ2_dA1 <- W_2
   dC_dA1 <- t(W_2) %*% dC_dZ2
   
   dC_dB2 <- matrix(rowSums(dC_dW2), ncol = 1)
   
-  return(list(dC_dW2 = dC_dW2, dC_dB2 = dC_dB2, dC_dA1 = dC_dA1))
-}
-  
-backpropagation_1 <- function(dC_dA1, A_1, A_0, W_1){
-  
+  # Layer 1
   dA1_dZ1 <- A_1 * (1 - A_1)
   dC_dZ1 <- dC_dA1 * dA1_dZ1
   
@@ -139,20 +114,33 @@ backpropagation_1 <- function(dC_dA1, A_1, A_0, W_1){
   dC_dW1 <- dC_dZ1 %*% t(dZ1_dW1)
   
   dC_dB1 <- matrix(rowSums(dC_dW1), ncol = 1)
-    
-  return(list(dC_dW1 = dC_dW1, dC_dB1 = dC_dB1))
-}
   
-# bp3 <- backpropagation_3(Y_hat = Y_hat$Y_hat, Y = Y, m = m, A_2 = Y_hat$hidden$A_2, W_3 = W$W_3) 
-# bp2 <- backpropagation_2(dC_dA2 = bp3$dC_dA2, A_1 = Y_hat$hidden$A_1, A_2 = Y_hat$hidden$A_2, W_2 = W$W_2) 
-# bp1 <- backpropagation_1(dC_dA1 = bp2$dC_dA1, A_1 = Y_hat$hidden$A_1, A_0 = A_0, W_1 = W$W_1) 
+  return(list(dC_dW3 = dC_dW3, 
+              dC_dW2 = dC_dW2,
+              dC_dW1 = dC_dW1,
+              dC_dB3 = dC_dB3,
+              dC_dB2 = dC_dB2,
+              dC_dB1 = dC_dB1))
+}
+
+
+#' Setup data
+Dat <- Dat2
+A_0 <- t(Dat[,c("x1", "x2")])
+Y <- matrix(Dat$y, nrow = 1)
+
+#' Define and Initialize network
+n = c(2, 5, 5, 1) # Nodes per layer (and input)
+W <- list(W_1 = matrix(rnorm(n = n[2] * n[1]), nrow = n[2], ncol = n[1]),
+          W_2 = matrix(rnorm(n = n[3] * n[2]), nrow = n[3], ncol = n[2]),
+          W_3 = matrix(rnorm(n = n[4] * n[3]), nrow = n[4], ncol = n[3]))
+B <- list(B_1 = matrix(rnorm(n = n[2]), nrow = n[2], ncol = 1),
+          B_2 = matrix(rnorm(n = n[3]), nrow = n[3], ncol = 1),
+          B_3 = matrix(rnorm(n = n[4]), nrow = n[4], ncol = 1))
 
 #' Setup training
-
-  # global W3, W2, W1, b3, b2, b1
-
-epochs <- 2000 # training for 1000 iterations
-lr <- 1 # set learning rate to 0.1
+epochs <- 20000 # training for 1000 iterations
+lr <- 0.5 # set learning rate to 0.1
 Cost <- NULL
 
 for(epoch in 1:epochs){
@@ -163,20 +151,27 @@ for(epoch in 1:epochs){
   cost <- cross_entropy_binom_cost(y = Y, y_hat = Y_hat$Y_hat)
   Cost <- c(Cost, cost)
   
-  # Calculate gradient
-  bp3 <- backpropagation_3(Y_hat = Y_hat$Y_hat, Y = Y, m = m, A_2 = Y_hat$hidden$A_2, W_3 = W$W_3)
-  bp2 <- backpropagation_2(dC_dA2 = bp3$dC_dA2, A_1 = Y_hat$hidden$A_1, A_2 = Y_hat$hidden$A_2, W_2 = W$W_2)
-  bp1 <- backpropagation_1(dC_dA1 = bp2$dC_dA1, A_1 = Y_hat$hidden$A_1, A_0 = A_0, W_1 = W$W_1)
+  
+  Grad <- backpropagation(Y_hat = Y_hat$Y_hat, 
+                          Y = Y, 
+                          m = m, 
+                          A_2 = Y_hat$hidden$A_2,
+                          A_1 = Y_hat$hidden$A_2,
+                          A_0 = A_0,
+                          W_3 = W$W_3,
+                          W_2 = W$W_2,
+                          W_1 = W$W_1)
+  
   
   # Update weights
-  W$W_3 <- W$W_3 - (lr * bp3$dC_dW3)
-  W$W_2 <- W$W_2 - (lr * bp2$dC_dW2)
-  W$W_1 <- W$W_1 - (lr * bp1$dC_dW1)
+  W$W_3 <- W$W_3 - (lr * Grad$dC_dW3)
+  W$W_2 <- W$W_2 - (lr * Grad$dC_dW2)
+  W$W_1 <- W$W_1 - (lr * Grad$dC_dW1)
   
   # Update biases
-  B$B_3 <- B$B_3 - (lr * bp3$dC_dB3)
-  B$B_2 <- B$B_2 - (lr * bp2$dC_dB2)
-  B$B_1 <- B$B_1 - (lr * bp1$dC_dB1)
+  B$B_3 <- B$B_3 - (lr * Grad$dC_dB3)
+  B$B_2 <- B$B_2 - (lr * Grad$dC_dB2)
+  B$B_1 <- B$B_1 - (lr * Grad$dC_dB1)
   
   if(epoch %% 100 == 0){
     cat(paste0("Cost in epoch ", epoch, " is: ", cost, "\n"))
@@ -184,12 +179,12 @@ for(epoch in 1:epochs){
   
 }
 
-
+plot(1:length(Cost), Cost)
 Y_hat <- ff_nnet(A_0 = A_0, W = W, B = B, g = g_logistic)
 summary(as.vector(Y_hat$Y_hat))
+table(obs = as.vector(Y), pred = as.vector(round(Y_hat$Y_hat)))
 
-
-Dat2 %>%
+Dat %>%
   mutate(y_hat = as.vector(Y_hat$Y_hat)) %>%
   mutate(y_pred = round(y_hat)) %>%
   mutate(res = "correct_red") %>%
